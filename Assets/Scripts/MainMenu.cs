2f1c3bd62713cdc2de8c64c3bc928a4764b5e81c
Wooneh <sam.woon@hotmail.com>
2019-09-27 21:09:11 +1200

    Added menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public bool isStory;
+    public bool isEndless;
+
+    // Trigger when mouse is clicked
+    void OnMouseUp()
+    {
+        if (isStory)
+        {
+            Debug.Log("Story Mode");
+        }
+        if (isEndless)
+        {
+            Debug.Log("Endless Mode");
+        }
+    }
+}
