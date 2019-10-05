2cc2c6e41b876a801d13b1a2fba9a65f2383960c
Frank Wang Ma <frankwangma@gmail.com>
2019-10-05 13:02:25 +1300

    Changed dialogue box script to different style

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public Text nameText;
+    public Text dialogueText;
+
+    public Animator animator;
+
+    private Queue<string> Sentences;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        animator.SetBool("isOpen", true);
+        nameText.text = dialogue.name;
+
+        Sentences.Clear();
+        foreach (string sentence in dialogue.sentences)
+        {
+            Sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if(Sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = Sentences.Dequeue();
+        dialogueText.text = sentence;
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence(string sentence)
+    {
+        dialogueText.text = "";
+        
+        foreach ( char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            // wait a single frame
+            yield return null;
+        }
+    }
+
+    void EndDialogue()
+    {
+        animator.SetBool("isOpen", false);
+    }
+   
+}
