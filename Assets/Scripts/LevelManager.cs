de55be2c9a64e8369dcca75dc4a08ac75b4ace4d
XiaoDangJia <zhuguangya98@gmail.com>
2019-10-05 18:44:52 +1300

    basic spawner implemented level is pretty hard now

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -4,16 +4,72 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
+    private const bool SHOW_COLLIDER = true;
 
     public static LevelManager Instance {set;get;}
 
+    //level spawning
+    private const float DISTANCE_BEFORE_SPAWN = 100.0f;
+    private const int INITIAL_SEGMENTS = 10;
+    private const int MAX_SEGMENTS_ON_SCREEN = 15;
+    private Transform cameraContainer;
+    private int amountOfActiveSegments;
+    private int continiousSegments;
+    private int currentSpawnZ;
+    private int currentLevel;
+    private int y1, y2, y3;
 
-
+    //list of pieces
     public List<Piece> ramps = new List<Piece>();
     public List<Piece> longblocks = new List<Piece>();
     public List<Piece> jumps = new List<Piece>();
     public List<Piece> slides = new List<Piece>();
+    [HideInInspector]
     public List<Piece> pieces = new List<Piece>();//all the pieces in the pool
+
+    //list of segments
+    public List<Segement> availableSegments = new List<Segement>();
+    public List<Segement> availableTransitions = new List<Segement>();
+    [HideInInspector]
+    public List<Segement> segments = new List<Segement>();
+
+    //GamePlay
+    private bool isMoving = false;
+
+    private void Awake()
+    {
+        Instance = this;
+        cameraContainer = Camera.main.transform;
+        currentSpawnZ = 0;
+        currentLevel = 0;
+    }
+
+    public Segement GetSegment(int id, bool transition)
+    {
+        Segement s = null;
+        s = segments.Find(x => x.SegId == id && x.transition == transition && !x.gameObject.activeSelf);
+
+        if( s == null)
+        {
+            GameObject go = Instantiate((transition)?availableTransitions[id].gameObject : availableSegments[id].gameObject) as GameObject;
+            s = go.GetComponent<Segement>();
+
+            s.SegId = id;
+            s.transition = transition;
+
+            segments.Insert(0, s);
+
+        }
+        else
+        {
+            segments.Remove(s);
+            segments.Insert(0, s);
+        }
+
+        return s;
+
+    }
+    
 
     public Piece GetPiece(PieceType pt, int visualIndex)
     {
@@ -46,7 +102,65 @@ public class LevelManager : MonoBehaviour
     // Start is called before the first frame update\
     void Start()
     {
-        
+        for (int i = 0; i < INITIAL_SEGMENTS; i++)
+        {
+            //generate a segment
+            GenerateSegment();
+        }
+    }
+
+    private void GenerateSegment()
+    {
+        SpawnSegment();
+        if (Random.RandomRange(0f, 1f) < (continiousSegments * 0.25f))
+        {
+            //spawn transition seg
+            continiousSegments = 0;
+            SpawnTransition();
+        }
+        else
+        {
+            continiousSegments++;
+        }
+ 
+    }
+
+    private void SpawnSegment()
+    {
+        List<Segement> possibleSeg = availableSegments.FindAll(x => x.beginY1 == y1 || x.beginY2 == y2 || x.beginY3 == y3);
+        int id = Random.Range(0, possibleSeg.Count);
+
+        Segement s = GetSegment(id, false);
+
+        y1 = s.endY1;
+        y2 = s.endY2;
+        y3 = s.endY3;
+
+        s.transform.SetParent(transform);
+        s.transform.localPosition = Vector3.forward * currentSpawnZ;
+
+        currentSpawnZ += s.length;
+        amountOfActiveSegments++;
+        s.Spawn();
+    }
+
+    private void SpawnTransition()
+    {
+        List<Segement> possibleTransition = availableTransitions.FindAll(x => x.beginY1 == y1 || x.beginY2 == y2 || x.beginY3 == y3);
+        int id = Random.Range(0, possibleTransition.Count);
+
+        Segement s = GetSegment(id, false);
+
+        y1 = s.endY1;
+        y2 = s.endY2;
+        y3 = s.endY3;
+
+        s.transform.SetParent(transform);
+        s.transform.localPosition = Vector3.forward * currentSpawnZ;
+
+        currentSpawnZ += s.length;
+        amountOfActiveSegments++;
+        s.Spawn();
     }
 
     // Update is called once per frame
