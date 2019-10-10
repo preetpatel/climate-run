62ed9fdabc97e64c3f2366db63021b0e69310f71
Wooneh <sam.woon@hotmail.com>
2019-10-10 17:36:54 +1300

    Added level 4 - Ice breaking behind player

---
+++ Assets/Scripts/Antarctica/Icebreaker.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Icebreaker : MonoBehaviour
+{
+    private Transform lookAt; // our player
+    private Vector3 offset;
+
+    private void Start()
+    {
+        lookAt = GameObject.FindGameObjectWithTag("Player").transform;
+        offset = new Vector3(0, 0, 5.0f);
+    }
+
+    // Ice breaks as player moves forward
+    private void LateUpdate()
+    {
+        if (lookAt.position.z > 10) // wait for player to move
+        {
+            Vector3 desiredPos = lookAt.position + offset;
+            Vector3 targetPos = Vector3.zero;
+            targetPos.z = Mathf.Lerp(transform.position.z, desiredPos.z, Time.deltaTime);
+            targetPos.y = transform.position.y;
+            targetPos.x = transform.position.x;
+            transform.position = targetPos;
+        }
+    }
+}
