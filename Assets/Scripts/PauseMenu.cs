ebb30a2a5fb049de8109f7418e20ebee480be427
Preet <iam@preetpatel.com>
2019-10-11 11:42:54 +1300

    added basic pause menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public static bool isPaused;
+    public GameObject pauseMenuUI;
+    public GameObject pauseButtonUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+                PressResumeButtonHandler();
+            else
+                PressPauseButtonHandler();
+        }
+      
+
+        
+    }
+
+    public void PressPauseButtonHandler()
+    {
+        Debug.Log("Hi");
+        pauseMenuUI.SetActive(true);
+        pauseButtonUI.SetActive(false);
+        Time.timeScale = 0f;
+        isPaused = true;
+
+    }
+
+    public void PressResumeButtonHandler()
+    {
+        pauseMenuUI.SetActive(false);
+        pauseButtonUI.SetActive(true);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+}
