f3d0b0b5b30dab07249571c11068db0ed7b64697
margeobur <margeobur@gmail.com>
2019-10-14 14:25:47 +1300

    added new PieceTypes to scripts

--- Assets/Scripts/Forest/Piece.cs
+++ Assets/Scripts/Forest/Piece.cs
@@ -8,7 +8,10 @@ public enum PieceType
     ramp = 0,
     longblock = 1,
     jump = 2,
-    slide = 3
+    slide = 3,
+    treeRow = 4,
+    longRamp = 5,
+    fallenTree = 6
 }
 
 public class Piece : MonoBehaviour
