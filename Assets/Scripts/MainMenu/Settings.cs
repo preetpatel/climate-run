15657994adbe5c2785e91f585e2d2583e51a4dc4
Frank Wang Ma <frankwangma@gmail.com>
2019-10-17 18:55:15 +1300

    Added some buttons to the settings

---
+++ Assets/Scripts/MainMenu/Settings.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Settings : MonoBehaviour
+{
+
+    public Button musicButton;
+    public Button sfxButton;
+    
+    private bool isMusicPressed = false;
+    private bool isSfxPressed = false;
+
+    public void onMusicPress()
+    {
+        isMusicPressed = buttonPressed(musicButton, isMusicPressed);
+    }
+
+    public void onSFXPress()
+    {
+        isSfxPressed = buttonPressed(sfxButton, isSfxPressed);
+    }
+
+    private bool buttonPressed(Button buttonPressed, bool buttonStatus)
+    {
+        Text text = buttonPressed.GetComponentInChildren<Text>();
+        Image image = buttonPressed.image;
+        if (buttonStatus)
+        {
+            buttonPressed.image.color = Color.green;
+            text.text = "On";
+            buttonStatus = false;
+        }
+        else
+        {
+            buttonPressed.image.color = Color.red;
+            text.text = "Off";
+            buttonStatus = true;
+        }
+        return buttonStatus;
+    }
+}
