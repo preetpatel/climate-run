74bbb231805603a531c953e5efaaebcfc527cfe3
margeobur <margeobur@gmail.com>
2019-10-19 14:19:21 +1300

    added simple firetruck segment

---
+++ Assets/Scripts/Forest/FireTruckAction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireTruckAction : MonoBehaviour
+{
+    private bool triggeredAlready = false;
+
+    internal void doWaterSpray()
+    {
+        if (!triggeredAlready)
+        {
+            triggeredAlready = true;
+            Time.timeScale = 0.5f;
+        }
+        
+    }
+}
