fe5ab14d2830dd33acd74d48e12b8e1f0f6b8002
margeobur <margeobur@gmail.com>
2019-10-20 00:25:51 +1300

    made fire truck segment larger and added growing bushes

--- Assets/Scripts/Forest/FireTruckAction.cs
+++ Assets/Scripts/Forest/FireTruckAction.cs
@@ -10,16 +10,19 @@ public class FireTruckAction : MonoBehaviour
 {
     private bool triggeredAlready = false;
     private bool transitioning = false;
+    private bool growingBushes = false;
     private int framesSinceTrigger = 0;
     private int framesSinceExtinguish = 0;
+    private int framesSinceBushGrow = 0;
     private Dictionary<SplashFireCombo, bool> extinguishableFireParticles;
+    private Dictionary<GrowingBush, bool> grownBushes;
 
     internal void doWaterSpray()
     {
         if (!triggeredAlready)
         {
             triggeredAlready = true;
-            Time.timeScale = 0.3f;
+            Time.timeScale = 0.4f;
             transitioning = true;
 
             extinguishableFireParticles = new Dictionary<SplashFireCombo, bool>();
@@ -28,6 +31,13 @@ public class FireTruckAction : MonoBehaviour
             {
                 extinguishableFireParticles.Add(fireParticles[i], false);
             }
+
+            grownBushes = new Dictionary<GrowingBush, bool>();
+            GrowingBush[] bushes = GetComponentsInChildren<GrowingBush>();
+            for (int i = 0; i < bushes.Length; i++)
+            {
+                grownBushes.Add(bushes[i], false);
+            }
         }
     }
 
@@ -35,16 +45,18 @@ public class FireTruckAction : MonoBehaviour
     {
         if(transitioning)
         {
-            if (framesSinceTrigger > 160)
+            if (framesSinceTrigger > 180)
             {
                 transitioning = false;
+                growingBushes = true;
+                framesSinceExtinguish = 0;
                 Time.timeScale = 1.0f;
                 return;
             }
 
             framesSinceExtinguish++;
 
-            if (framesSinceExtinguish >= 15)
+            if (framesSinceExtinguish >= 7)
             {
                 framesSinceExtinguish = 0;
 
@@ -55,13 +67,42 @@ public class FireTruckAction : MonoBehaviour
                     {
                         extinguishableFireParticles[particle] = true;
                         particle.Trigger();
-
                         break;
                     }
                 }
             }
 
             framesSinceTrigger++;
+        }
+        else if (growingBushes)
+        {
+            bool allBushesGrown = false;
+
+            framesSinceBushGrow++;
+
+            if (framesSinceBushGrow >= 7)
+            {
+                allBushesGrown = true;
+                foreach (KeyValuePair<GrowingBush, bool> bushGrown in grownBushes)
+                {
+                    GrowingBush bush = bushGrown.Key;
+                    if (!bushGrown.Value)
+                    {
+                        Debug.Log("There's a bush to grow :)");
+                        grownBushes[bush] = true;
+                        bush.Trigger();
+                        allBushesGrown = false;
+                        break;
+                    }
+                }
+
+                framesSinceBushGrow = 0;
+            }
+
+            if (allBushesGrown)
+            {
+                growingBushes = false;
+            }
         }
     }
 }
---
+++ Assets/Scripts/Forest/GrowingBush.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrowingBush : MonoBehaviour
+{
+    private Animator animator;
+    
+    private void Awake()
+    {
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    public void Trigger()
+    {
+        Debug.Log("triggering a bush");
+        animator.SetTrigger("Grow");
+    }
+}
