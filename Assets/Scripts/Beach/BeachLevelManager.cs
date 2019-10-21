64920ddaebc16d4afa6c00a935a8c51c86173631
George-E-M <georgeedwinmcerlean@gmail.com>
2019-10-21 18:06:56 +1300

    Edited Garbage Numbers

--- Assets/Scripts/Beach/BeachLevelManager.cs
+++ Assets/Scripts/Beach/BeachLevelManager.cs
@@ -100,7 +100,7 @@ public class BeachLevelManager : MonoBehaviour
             score += (Time.deltaTime * modifier);
             scoreText.text = "Score : " + score.ToString("0");
             timeSinceGarbageCollected += Time.deltaTime;
-            if(timeSinceGarbageCollected > 5.0f)
+            if(timeSinceGarbageCollected > 3.5f)
             {
                 garbageCollected = false;
                 timeSinceGarbageCollected = 0.0f;
@@ -139,7 +139,7 @@ public class BeachLevelManager : MonoBehaviour
         garbageText.text = "Garbage : " + garbage.ToString();
         garbageCollected = true;
         float garbMulti = TrashSpawner.garbageMultiplier;
-        TrashSpawner.garbageMultiplier = Mathf.Clamp(garbMulti -= 0.25f, 0.0f, 1.0f);
+        TrashSpawner.garbageMultiplier = Mathf.Clamp(garbMulti -= 0.2f, 0.0f, 1.0f);
         pollutionSlide.value = TrashSpawner.garbageMultiplier;
     }
     
