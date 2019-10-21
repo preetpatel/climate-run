4eef4bdf18fad6a8ed3d7c6d3b5bd8fc48163287
NoMacAlive <37617906+NoMacAlive@users.noreply.github.com>
2019-10-21 19:34:04 +1300

    modified the alive panel and made the background of the text more intuitive

--- Assets/Scripts/Forest/ForestLevelManager.cs
+++ Assets/Scripts/Forest/ForestLevelManager.cs
@@ -53,8 +53,8 @@ public class ForestLevelManager : MonoBehaviour
 		playerMotor = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMotor>();
 		cameraMotor = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraMotor>();
         compMotor = GameObject.FindGameObjectWithTag("Companion").GetComponent<GorillaMotor>();
-        scoreText.text = "Score : " + score.ToString("0");
-		seedCountText.text = "Seeds : " + seeds.ToString();
+        scoreText.text = score.ToString("0");
+		seedCountText.text = seeds.ToString();
 
         isEndless = SceneController.getIsEndless();
 
@@ -105,7 +105,7 @@ public class ForestLevelManager : MonoBehaviour
 		if (isGameStarted)
 		{
 			score += (Time.deltaTime * modifier);
-			scoreText.text = "Score : " + score.ToString("0");
+			scoreText.text = score.ToString("0");
 
             if (!isEndless)
             {
@@ -127,7 +127,7 @@ public class ForestLevelManager : MonoBehaviour
 	public void getSeeds()
 	{
 		seeds++;
-		seedCountText.text = "Seeds : " + seeds.ToString();
+		seedCountText.text = seeds.ToString();
 	}
 
 	public IEnumerator updateLives(float livesAmount)
